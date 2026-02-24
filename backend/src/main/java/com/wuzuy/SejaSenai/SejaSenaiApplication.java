a0e6b1784a8a5083a79a06f890f28164503d592b
Lucas Matheus <89390605+Wuzuy@users.noreply.github.com>
2026-02-24 11:45:42 -0300

    Inicialização e configuração do SpringBoot

---
+++ backend/src/main/java/com/wuzuy/SejaSenai/SejaSenaiApplication.java
@@ -0,0 +1,13 @@
+package com.wuzuy.SejaSenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejaSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejaSenaiApplication.class, args);
+	}
+
+}
