a8c4f8872e5a61b6677debe97b8ec1ee6d337ea3
Lucas Matheus <89390605+Wuzuy@users.noreply.github.com>
2026-02-25 10:57:15 -0300

    Interação de Login do usuário no backend
    Foi adicionado classes de controle, DTO, modelos e funções a integrar futuramente com o front-end.

---
+++ backend/src/main/java/com/wuzuy/sejasenai/model/Usuario.java
@@ -0,0 +1,88 @@
+package com.wuzuy.sejasenai.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "senai_usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeCompleto;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column
+    private String telefone;
+
+    @Column
+    private LocalDate dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate nascimento) {
+        this.dataNascimento = nascimento;
+    }
+}
