076cc93fecf6e4bde0526b48fc7c83272836489f
Lucas Matheus <89390605+Wuzuy@users.noreply.github.com>
2026-02-26 16:45:06 -0300

    Criação de Repositórios, Modelos e Controllers
    Falta finalizar os Controllers e moldar os repositórios

---
+++ backend/src/main/java/com/wuzuy/sejasenai/Controller/InscricaoController.java
@@ -0,0 +1,12 @@
+package com.wuzuy.sejasenai.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/inscricoes")
+@CrossOrigin(origins = "*")
+public class InscricaoController {
+    // Todo Terminar
+}
---
+++ backend/src/main/java/com/wuzuy/sejasenai/Controller/UnidadeController.java
@@ -0,0 +1,12 @@
+package com.wuzuy.sejasenai.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/unidades")
+@CrossOrigin(origins = "*")
+public class UnidadeController {
+    // Todo Terminar
+}
