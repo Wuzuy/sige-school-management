52d0f60f6d86c2667b4a7052cede2163240c51c0
Lucas Matheus <89390605+Wuzuy@users.noreply.github.com>
2026-03-02 16:26:47 -0300

    Mudanças de interatividade
    Adicionei confirmação de senha

--- backend/src/main/java/com/wuzuy/sejasenai/config/SecurityConfig.java
+++ backend/src/main/java/com/wuzuy/sejasenai/config/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.wuzuy.sejasenai.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     // Define o encriptador de senhas como um Bean para ser usado no Controller
@@ -19,8 +19,28 @@ public class SecurityConfig {
     // Configura as rotas para permitir acesso sem autenticacao por enquanto
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+<<<<<<< Updated upstream
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+=======
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // Permite a pré-verificação do navegador (CORS)
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/api/usuarios/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/api/cursos/**", "/api/unidades/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/cursos/**", "/api/unidades/**").hasAuthority("ROLE_ADMIN")
+
+                        .requestMatchers("/api/inscricoes/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(basic -> {});
+
+>>>>>>> Stashed changes
         return http.build();
     }
 }
